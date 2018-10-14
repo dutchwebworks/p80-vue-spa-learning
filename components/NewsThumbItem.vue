be1e29d536b2750ca3c94c074a0962579d01a704
Dennis Burger <dennis@dutchwebworks.nl>
2018-10-14 14:03:47 +0200

    Moved to separate component.

---
+++ components/NewsThumbItem.vue
@@ -0,0 +1,62 @@
+<template>
+    <article class="news__item">
+        <nuxt-link
+            v-bind:to="'/news/' + newsItem.id + '/' + newsItem.date + '/' + $options.filters.slugify(newsItem.title)">
+            <img
+                class="news__item-thumbnail"
+                v-bind:src="newsItem.thumbnail"
+                v-bind:alt="newsItem.title">
+        </nuxt-link>				
+
+        <h3 class="news__item-title">
+            <nuxt-link
+                v-bind:to="'/news/' + newsItem.id + '/' + newsItem.date + '/' + $options.filters.slugify(newsItem.title)">
+                {{ newsItem.title }}
+            </nuxt-link>
+        </h3>
+
+        <p class="news__item-date"><small>{{ newsItem.dateNice }}</small></p>
+    </article>
+</template>
+
+<script>
+    export default {
+        name: "NewsItem",
+        props: ["newsItem"]
+        // props: {
+        //     newsItem: {
+        //         type: Object,
+        //         required: true
+        //     }
+        // }
+    };
+</script>
+
+<style lang="scss">
+	.news__item-title {
+        color: map-get($colors, 02);
+        
+        a {
+            color: inherit;
+            text-decoration: none;
+
+            &:hover {
+                color: map-get($colors, 01);
+            }
+        }
+	}
+
+	.news__item-thumbnail {
+		max-width: 100%;
+		box-shadow: 0 10px 25px 0 rgba(0, 0, 0, 0.25);
+	}
+
+	.news__item-description {
+		line-height: 1.6;
+	}
+
+	.news__item-date {
+		font-size: 18px;
+		color: map-get($colors, 01);
+	}
+</style>
--- pages/news/index.vue
+++ pages/news/index.vue
@@ -3,35 +3,23 @@
 		<h1 class="heading">News</h1>
 
 		<section class="news__items">
-			<article
-				class="news__item" 
+			<NewsThumbItem
 				v-for="newsItem in newsItems"
+				v-bind:newsItem="newsItem"
 				v-bind:key="newsItem.id">
-
-                <nuxt-link
-                    v-bind:to="'/news/' + newsItem.id + '/' + newsItem.date + '/' + $options.filters.slugify(newsItem.title)">
-                    <img
-                        class="news__item-thumbnail"
-                        v-bind:src="newsItem.thumbnail"
-                        v-bind:alt="newsItem.title">
-                </nuxt-link>				
-
-				<h3 class="news__item-title">
-                    <nuxt-link
-                        v-bind:to="'/news/' + newsItem.id + '/' + newsItem.date + '/' + $options.filters.slugify(newsItem.title)">
-                        {{ newsItem.title }}
-                    </nuxt-link>
-                </h3>
-
-				<p class="news__item-date"><small>{{ newsItem.dateNice }}</small></p>
-			</article>
+			</NewsThumbItem>
 		</section>
 	</section>
 </template>
 
 <script>
+	import NewsThumbItem from "@/components/NewsThumbItem";
+
     export default {
-        name: "News",
+		name: "News",
+		components: {
+			NewsThumbItem
+		},
         computed: {
             newsItems() {
                 return this.$store.getters.news;
@@ -44,37 +32,10 @@
     .news {
         font-family: $font-custom;
 	}
-	
+
 	.news__items {
 		display: grid;
 		grid-gap: 40px;
 		grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
-	}
-
-	.news__item-title {
-        color: map-get($colors, 02);
-        
-        a {
-            color: inherit;
-            text-decoration: none;
-
-            &:hover {
-                color: map-get($colors, 01);
-            }
-        }
-	}
-
-	.news__item-thumbnail {
-		max-width: 100%;
-		box-shadow: 0 10px 25px 0 rgba(0, 0, 0, 0.25);
-	}
-
-	.news__item-description {
-		line-height: 1.6;
-	}
-
-	.news__item-date {
-		font-size: 18px;
-		color: map-get($colors, 01);
 	}
 </style>
